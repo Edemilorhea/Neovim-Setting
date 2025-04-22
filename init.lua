213391c6da0355787820b119b799ca9b9b2f4f3e
Edemilorhea <leon8847@gmail.com>
2025-04-22 16:00:05 +0800

    新版架構

--- init.lua
+++ init.lua
@@ -9,7 +9,19 @@ end
 
 require("PluginsList")
 
+-- ✅ Plugin 個別設定（模組化初始化）
+require("plugin.config.flash").setup()
+require("plugin.config.nvimtree").setup()
+require("plugin.config.peek").setup()
+require("plugin.config.imselect").setup()
+require("plugin.config.lualine").setup()
+require("plugin.config.telescope").setup()
+require("plugin.config.surround").setup()
+
+-- ✅ 只有非 VSCode 模式才載入 LSP 與 Treesitter
 if not is_vscode then
+    require("plugin.config.treesitter").setup()
+    require("plugin.config.ufo").setup()
     require("plugin.config.mason").enable()
     require("plugin.config.lsp").setup()
 end

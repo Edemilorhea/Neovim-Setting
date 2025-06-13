32b62ef58fd65681e7719a9d346c073f773f2ef6
TC <leon8847@gmail.com>
2025-06-13 23:14:12 +0800

    C-o C-i 也加上ESC

--- lua/keymap/general.lua
+++ lua/keymap/general.lua
@@ -5,6 +5,14 @@ function M.setup()
 
     vim.keymap.set("n", "<Esc>", "<Esc>:nohlsearch<CR>", { silent = true })
 
+    vim.keymap.set("n", "<C-u>", function()
+        vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes("<Esc>", true, false, true), "n", false)
+    end, { noremap = true, silent = true })
+
+    vim.keymap.set("n", "<C-i>", function()
+        vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes("<Esc>", true, false, true), "n", false)
+    end, { noremap = true, silent = true })
+
     -- Redo with <Esc>
     vim.keymap.set("n", "<C-r>", function()
         vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes("<Esc>", true, false, true), "n", false)
